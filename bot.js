3fdc7ad5a5cc2b153dc13900913e8cff87cc6f28
Jnaby <44756627+kwras@users.noreply.github.com>
2018-11-30 12:24:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "2968"// البرفكس هنا
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -31,47 +31,68 @@ client.on('ready', () => {
 
 
 });
+
+
+
+
  
- client.on('ready',async () => {
-console.log("Starting..");
-let g = client.guilds.get("499633648866164737");
-let c = g.channels.get("500651445327233034");
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(!g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log("Failed To Join:\n The Channel Type isn't \"text\"");
-}
+ 
+
+var prefix = "S"
+client.on('message', function(message) {
+    const myID = "444902575041675274";
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(message.content.startsWith(prefix + "setname")) {
+                if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setUsername(args);
+        message.channel.send('**SetName,Done!**').then(msg => {
+           msg.delete(500);
+          message.delete(500);
+        });
+    } else if(message.content.startsWith(prefix + "st")) {
+                if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args , 'https://twitch.tv/6xlez1');
+        message.channel.send('**Your Status** : `Streaming`').then(msg => {
+           msg.delete(500);
+          message.delete(500);
+        });
+    } else if(message.content.startsWith(prefix + "ply")) {
+                        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args);
+        message.channel.send('**Your Status** : `Playing`').then(msg => {
+           msg.delete(500);
+          message.delete(500);
+        });
+} else if(message.content.startsWith(prefix + "ls")) {
+                        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'LISTENING'});
+        message.channel.send('**Your Status** : `Listening`').then(msg => {
+           msg.delete(500);
+          message.delete(500);
+        });
+    } else if(message.content.startsWith(prefix + "wt")) {
+                        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'WATCHING'});
+        message.channel.send('**Your Status** : `Watching`').then(msg => {
+           msg.delete(500);
+          message.delete(500);
+        });
+    } else if(message.content.startsWith(prefix + "setavatar")) {
+                        if(message.author.id !== myID) return;
+        client.user.setAvatar(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+                if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+           msg.delete(500);
+          message.delete(500);
+        });
+    }
 });
- 
- 
-const adminprefix = "2968";
-const devs = ['506211849100984320'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'av')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else    
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
- 
-});
+
 
 
 
